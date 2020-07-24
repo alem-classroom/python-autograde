3438794648e49325d06813b9de485963d80e91b4
Zulbukharov <l1pt0n1905@gmail.com>
2020-07-24 18:31:38 +0600

    feat: remove silence

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,7 +60,8 @@ z=$(find $TEST -mindepth 1 -maxdepth 1 -type d -name "test*" -print0 | xargs -n 
 send_result(){
     data=$(jq -aRs . <<< ${5})
     # apikey user lesson status logs 
-    curl -s -X POST "https://lrn.dev/api/service/grade" -H "x-grade-secret: ${1}" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\":\"${2}\", \"lesson\":\"${3}\", \"status\": \"${4}\", \"logs\": ${data}}" > /dev/null
+    curl -s -X POST "https://lrn.dev/api/service/grade" -H "x-grade-secret: ${1}" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\":\"${2}\", \"lesson\":\"${3}\", \"status\": \"${4}\", \"logs\": ${data}}"
+    echo ""
 }
 
 pip install pytest > /dev/null
